02da729d76bc423653e2ce0e46dd4afdbe32642a
parnian <behdin.p@gmail.com>
2022-06-08 16:39:42 +0430

    Add iteration

--- main.py
+++ main.py
@@ -12,56 +12,69 @@ from utils import tab_printer
 def main(args):
     data_class = SiReNDataLoader(args.dataset, args.version)
     print('data loading...')
-    train, test = data_class.data_load()
-    train = train.astype({'userId': 'int64', 'movieId': 'int64'})
-    offset = args.offset
+    sum_eval_p = [0,0,0,0,0]
+    sum_eval_r = [0,0,0,0,0]
+    sum_eval_n = [0,0,0,0,0]
+    iteration = 5
+    for i in range(iteration):
+        train, test = data_class.data_load()
+        train = train.astype({'userId': 'int64', 'movieId': 'int64'})
+        offset = args.offset
 
-    edge_user = torch.tensor(train[train['rating'] > offset]['userId'].values - 1)
-    edge_item = torch.tensor(train[train['rating'] > offset]['movieId'].values - 1) + data_class.num_u
+        edge_user = torch.tensor(train[train['rating'] > offset]['userId'].values - 1)
+        edge_item = torch.tensor(train[train['rating'] > offset]['movieId'].values - 1) + data_class.num_u
 
-    edge_user_n = torch.tensor(train[train['rating'] <= offset]['userId'].values - 1)
-    edge_item_n = torch.tensor(train[train['rating'] <= offset]['movieId'].values - 1) + data_class.num_u
+        edge_user_n = torch.tensor(train[train['rating'] <= offset]['userId'].values - 1)
+        edge_item_n = torch.tensor(train[train['rating'] <= offset]['movieId'].values - 1) + data_class.num_u
 
-    edge_p = torch.stack((torch.cat((edge_user, edge_item), 0), torch.cat((edge_item, edge_user), 0)), 0)
-    data_p = Data(edge_index=edge_p)
+        edge_p = torch.stack((torch.cat((edge_user, edge_item), 0), torch.cat((edge_item, edge_user), 0)), 0)
+        data_p = Data(edge_index=edge_p)
 
-    edge_n = torch.stack((torch.cat((edge_user_n, edge_item_n), 0), torch.cat((edge_item_n, edge_user_n), 0)), 0)
-    data_n = Data(edge_index=edge_n)
+        edge_n = torch.stack((torch.cat((edge_user_n, edge_item_n), 0), torch.cat((edge_item_n, edge_user_n), 0)), 0)
+        data_n = Data(edge_index=edge_n)
 
-    args2 = parameter_parser()
-    tab_printer(args2)
-    edges2 = {
-        "positive_edges": torch.transpose(data_p.to_dict()["edge_index"], 0, 1).tolist(),
-        "negative_edges": torch.transpose(data_n.to_dict()["edge_index"], 0, 1).tolist(),
-        "ecount": 0,
-        "ncount": data_class.num_u + data_class.num_v,
-    }
-    trainer = SignedGCNTrainer(args2, edges2)
-    trainer.setup_dataset()
-    z_list = trainer.create_and_train_model()
+        args2 = parameter_parser()
+        tab_printer(args2)
+        edges2 = {
+            "positive_edges": torch.transpose(data_p.to_dict()["edge_index"], 0, 1).tolist(),
+            "negative_edges": torch.transpose(data_n.to_dict()["edge_index"], 0, 1).tolist(),
+            "ecount": 0,
+            "ncount": data_class.num_u + data_class.num_v,
+        }
+        trainer = SignedGCNTrainer(args2, edges2)
+        trainer.setup_dataset()
+        z_list = trainer.create_and_train_model()
 
-    data_class.train = train
-    data_class.test = test
+        data_class.train = train
+        data_class.test = test
+        for EPOCH in range(len(z_list)):
+            emb = z_list[EPOCH]
+            emb_u, emb_v = torch.split(emb, [data_class.num_u, data_class.num_v])
+            emb_u = emb_u.cpu().detach()
+            emb_v = emb_v.cpu().detach()
+            r_hat = emb_u.mm(emb_v.t())
+            reco = gen_top_k(data_class, r_hat)
+            eval_ = ev(data_class, reco, args)
+            eval_.precision_and_recall()
+            eval_.normalized_DCG()
+            file = open("output.txt", "a")
+            file.write(f"""***************************************************************************************
+            /* Recommendation Accuracy */
+            N :: {eval_.N}
+            Precision at :: {eval_.N}, {eval_.p['total'][eval_.N - 1]}
+            Recall at [10, 15, 20] :: {eval_.r['total'][eval_.N - 1]}
+            nDCG at [10, 15, 20] :: {eval_.nDCG['total'][eval_.N - 1]}
+            ***************************************************************************************""")
+        sum_eval_p += eval_.p['total'][eval_.N - 1]
+        sum_eval_r += eval_.r['total'][eval_.N - 1]
+        sum_eval_n += eval_.nDCG['total'][eval_.N - 1]
+    sum_eval_p = [x/iteration for x in sum_eval_p]
+    sum_eval_r = [x/iteration for x in sum_eval_r]
+    sum_eval_n = [x/iteration for x in sum_eval_n]
 
-    for EPOCH in range(len(z_list)):
-        emb = z_list[EPOCH]
-        emb_u, emb_v = torch.split(emb, [data_class.num_u, data_class.num_v])
-        emb_u = emb_u.cpu().detach()
-        emb_v = emb_v.cpu().detach()
-        r_hat = emb_u.mm(emb_v.t())
-        reco = gen_top_k(data_class, r_hat)
-        eval_ = ev(data_class, reco, args)
-        eval_.precision_and_recall()
-        eval_.normalized_DCG()
-        file = open("output.txt", "a")
-        file.write(f"""***************************************************************************************
-/* Recommendation Accuracy */
-N :: {eval_.N}
-Precision at :: {eval_.N}, {eval_.p['total'][eval_.N - 1]}
-Recall at [10, 15, 20] :: {eval_.r['total'][eval_.N - 1]}
-nDCG at [10, 15, 20] :: {eval_.nDCG['total'][eval_.N - 1]}
-***************************************************************************************""")
-
+    file.write("Mean of Precision: {}".format(sum_eval_p))
+    file.write("Mean of Recall: {}".format(sum_eval_r))
+    file.write("Mean of nDCG: {}".format(sum_eval_n))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
